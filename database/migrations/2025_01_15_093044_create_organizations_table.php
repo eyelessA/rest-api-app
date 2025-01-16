faa08a90c1eb532ced829e193c10aff82f507de1
olegbugaev <bugaev0leg@yandex.ru>
2025-01-17 02:08:25 +0400

    feat: create GIN index

--- database/migrations/2025_01_15_093044_create_organizations_table.php
+++ database/migrations/2025_01_15_093044_create_organizations_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,6 +22,7 @@ return new class extends Migration
             $table->foreign('activity_id')->references('id')->on('activities');
             $table->timestamps();
         });
+        DB::statement('CREATE INDEX idx_name_trgm ON organizations USING gin (name gin_trgm_ops)');
     }
 
     /**
