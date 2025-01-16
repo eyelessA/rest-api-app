cc9e90afccdd0a8d093aa6fe0c51e84feaf0075c
olegbugaev <bugaev0leg@yandex.ru>
2025-01-16 23:48:08 +0400

    feat: create migrations

---
+++ database/migrations/2025_01_15_093043_create_activities_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('parent_id')->nullable()->index();
+            $table->foreign('parent_id')->references('id')->on('activities');
+            $table->unsignedInteger('level')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
