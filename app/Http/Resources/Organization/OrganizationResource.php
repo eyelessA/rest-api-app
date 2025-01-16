77ff1bc3d7d942960e17ff225cedd0cacb95ec28
olegbugaev <bugaev0leg@yandex.ru>
2025-01-16 23:51:02 +0400

    feat: create OrganizationResource.php

---
+++ app/Http/Resources/Organization/OrganizationResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Organization;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrganizationResource extends JsonResource
+{
+    public static $wrap = null;
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'phone_number' => $this->phone_number,
+            'building_id' => $this->building_id,
+            'activity' => $this->activity,
+        ];
+    }
+}
